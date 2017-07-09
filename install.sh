d1ee280c1121757f5a585bbb80c4ebc93dac8efd
tortuepin <kameyasu119@gmail.com>
2017-07-09 19:41:32 +0900

    color

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-files=("zshrc" "zshenv" "tmux.conf")
+files=("zshrc" "zshenv" "tmux.conf" "config/nvim")
 dir=`pwd`
 
-for f in $files; do
+for f in ${files[@]}; do
 	ln -s $dir/$f ~/.$f
 	echo linked $dir/$f ~/.$f
 done
